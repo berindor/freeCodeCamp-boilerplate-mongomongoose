910d9fbb4c3caa24b7982b44f23e6cf201520142
Beringer Dorottya <beringerdorottya@gmail.com>
2024-01-31 16:17:18 +0100

    set up mongoose again

--- myApp.js
+++ myApp.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 let mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGO_URI;
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
