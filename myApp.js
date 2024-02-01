75c3b548debbea2e025a853992c5428b89426860
Beringer Dorottya <beringerdorottya@gmail.com>
2024-02-01 11:39:21 +0100

    try to fix createAndSavePerson function

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@ Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = done => {
   const tamas = new Person({ name: 'Tamas', age: 37, favoriteFoods: ['pizza', 'beer'] });
-  await tamas.save(function (err) {
+  tamas.save(function (err) {
     console.log(err);
   });
   done(null /*, data*/);
