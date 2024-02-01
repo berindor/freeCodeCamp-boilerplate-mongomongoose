9f79d24d520343ef3bf5df76493dfb9ca94c3adf
Beringer Dorottya <beringerdorottya@gmail.com>
2024-02-01 10:54:01 +0100

    added personSchema

--- myApp.js
+++ myApp.js
@@ -3,7 +3,15 @@ let mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
 let Person;
+
+Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = done => {
   done(null /*, data*/);
