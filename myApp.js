430f2e16b8740d22ec84ab51cb2667236dd9fd91
Beringer Dorottya <beringerdorottya@gmail.com>
2024-02-01 16:06:52 +0100

    added content to findEditThenSave function

--- myApp.js
+++ myApp.js
@@ -59,6 +59,7 @@ const findPersonById = (personId, done) => {
     if (err) {
       console.log(err);
     } else {
+      console.log(data);
       done(null, data);
     }
   });
@@ -66,8 +67,21 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = 'hamburger';
-
-  done(null /*, data*/);
+  Person.findById(personId, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      data.favoriteFoods.push(foodToAdd);
+      data.save(function (err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+          done(null, data);
+        }
+      });
+    }
+  });
 };
 
 const findAndUpdate = (personName, done) => {
