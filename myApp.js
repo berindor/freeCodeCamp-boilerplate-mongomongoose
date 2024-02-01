7a3f3f31281616a9b82c658c8867a46e2aaead77
Beringer Dorottya <beringerdorottya@gmail.com>
2024-02-01 15:44:33 +0100

    added content to findOneByFood function

--- myApp.js
+++ myApp.js
@@ -45,7 +45,13 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findPersonById = (personId, done) => {
